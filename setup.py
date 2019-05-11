f40974974cc42029968d6fe75978b6b139574aec
Eric D Golinko <egolinko@Erics-MacBook-Pro.local>
2019-05-11 12:04:03 -0400

    setting up

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qGEL',
+      version='0.1',
+      description='A quick way to qGEL',
+      url='http://github.com/egolinko/qGELpy',
+      author='Eric Golinko',
+      author_email='egolinko@gmail.com',
+      license='MIT',
+      packages=['qGEL'],
+      zip_safe=False)
