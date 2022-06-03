c59bb9f965f6491a9aebb79e9dfd13b6d397ba8d
Eric Golinko <egolinko@gmail.com>
2022-06-03 02:51:54 +0000

    url change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.2",
     description="A quick way to qgel",
     long_description="A quick way to use the qgel embedding",
-    url="http://github.com/egolinko/qGELpy",
+    url="http://github.com/egolinko/qgel",
     author="Eric Golinko",
     author_email="egolinko@gmail.com",
     license="MIT",
